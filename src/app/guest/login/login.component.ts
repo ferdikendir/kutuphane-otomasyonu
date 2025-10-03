da79bb9575a6d74b69852425824bd5c507377a25
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-10-03 21:31:38 +0300

    (feature/Guest) - Added Login Component

--- src/app/guest/login/login.component.ts
+++ src/app/guest/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { Form, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: "library-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
   standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule
+  ],
 })
 export class LoginComponent {
+
+  private readonly formBuilder = inject(FormBuilder);
+
+  form: FormGroup = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+
+    this.form.markAllAsTouched();
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(this.form.value);
+
+  }
 }
