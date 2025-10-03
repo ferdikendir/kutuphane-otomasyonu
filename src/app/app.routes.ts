457cf964ee4059a578bb35ec11d809c9526cbc39
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-10-03 20:41:49 +0300

    (features/Guest) - Added auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '@guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -9,5 +10,6 @@ export const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('@modules/guest/guest.routes').then(m => m.authRoutes),
+    canActivate: [authGuard]
   }
 ];
