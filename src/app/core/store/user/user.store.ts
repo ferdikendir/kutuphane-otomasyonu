218367fa357eb396a5e2f469a89ff8d050012f1f
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-10-04 00:13:20 +0300

    (features/Dashboard) - Used store

---
+++ src/app/core/store/user/user.store.ts
@@ -0,0 +1,10 @@
+import { User } from "@models/user.model";
+import { Store } from "../base/store";
+
+const initialState: Partial<User> = {};
+
+const userStore = new Store<Partial<User>>(initialState);
+
+export const user = userStore.select();
+
+export const dispatchUser = (newState: Partial<User>): void => userStore.set(newState);
