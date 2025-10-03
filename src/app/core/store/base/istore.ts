4542ec85dbb6b571ca84731dee187ce376857a80
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-10-04 00:12:58 +0300

    (features/Dashboard) - Added base store

---
+++ src/app/core/store/base/istore.ts
@@ -0,0 +1,26 @@
+import { Signal } from "@angular/core";
+
+export interface IStore<T> {
+
+  // All state
+  select(): Signal<T>;
+
+  // Projected select
+  select<R>(project?: (state: T) => R): Signal<T | R>;
+
+  // Update state
+  update(mutator: (state: T) => T): void;
+
+  // Set state
+  set(newState: T): void;
+
+  // Get state
+  get(): T;
+
+  // Subscribe to state changes
+  onChange(callback: (state: T) => void): void;
+
+  // Clear state
+  clear(): void;
+
+}
