38b87a09c5052434452b22478668410efd3b2c47
ferdikendir <ferdi.kendir@std.yildiz.edu.tr>
2025-10-04 12:03:42 +0300

    (feature/Book) - Modified book component

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,14 +1,52 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { Book } from "@models/book.model";
+import { DateDiffPipe } from "@pipes/date-diff.pipe";
 
 @Component({
   selector: "library-book",
   templateUrl: "./book.component.html",
   styleUrls: ["./book.component.scss"],
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatTooltipModule,
+    DateDiffPipe,
+    MatButtonModule
+  ],
 })
 export class BookComponent {
   books = [
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
-    { title: "To Kill a Mockingbird", author: "Harper Lee" },
-    { title: "1984", author: "George Orwell" },
+    {
+      isbn: "978-3-16-148410-0",
+      name: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      edition: "1st",
+      year: 1925
+    },
+    {
+      isbn: "978-0-14-118263-6",
+      name: "1984",
+      author: "George Orwell",
+      edition: "1st",
+      year: 1949
+    },
+    {
+      isbn: "978-0-452-28423-4",
+      name: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      edition: "1st",
+      year: 1960
+    }
   ];
+
+
+  tableColumns: string[] = ['isbn', 'name', 'author', 'edition', 'year'];
+
+  dataSource: Book[] = this.books;
+
+  loading = signal(true);
+
 }
